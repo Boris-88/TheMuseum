2e01e6b3becd732a71aad22bfc418e12f02f9532
Boris Zverik <Boris.zverik@gmail.com>
2021-11-03 14:44:11 +0300

    init

---
+++ TheMuseum/TheMuseum/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TheMuseum
+//
+//  Created by Boris Zverik on 03.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
